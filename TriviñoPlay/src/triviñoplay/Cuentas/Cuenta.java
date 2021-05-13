0c551e1678279a9ad74895ba087a9c87b6b89cc7
Elías Ocque <elias.ocque2@gmail.com>
2021-05-13 18:39:32 -0400

    Creacion de la clase Cuenta

--- TriviñoPlay/src/triviñoplay/Cuentas/Cuenta.java
+++ TriviñoPlay/src/triviñoplay/Cuentas/Cuenta.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package triviñoplay.Cuentas;
+import javafx.scene.image.Image;
 
-/**
- *
- * @author Sudaii
- */
 public class Cuenta {
-    
+    boolean admin;
+    String nombre;
+    String email;
+    String contrasena;
+    Image imagenperfil;
 }
