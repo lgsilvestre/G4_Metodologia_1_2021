0ac7fa4dfdefd79968bd677bf24a8613b68f032f
Elías Ocque <elias.ocque2@gmail.com>
2021-05-25 19:17:18 -0400

    Modificado funcionamiento de archivos Media
    Media -> Multimedia

---
+++ TriviñoPlay/src/triviñoplay/Logica/Media/Multimedia.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triviñoplay.Logica.Media;
+
+import java.io.File;
+import java.io.IOException;
+import javafx.scene.image.Image;
+import java.util.ArrayList;
+import java.util.Calendar;
+import org.jaudiotagger.audio.*;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.*;
+
+
+/**
+ *
+ * @author Sudaii
+ */
+public class Multimedia {
+    File archivo;
+    AudioFile archivoMultimedia;
+    Tag metadata;
+    String titulo;
+    ArrayList<String> generos;
+    Calendar fechaLanzamiento;
+    Image portada;
+    int reproducciones, duracionSegundos;
+    
+    public Multimedia(String direccionArchivo, String direccionPortada){
+        this.archivo =  new File(direccionArchivo);
+        try {
+            archivoMultimedia = AudioFileIO.read(archivo);
+            metadata = archivoMultimedia.getTag();
+        } catch (CannotReadException | IOException | TagException | ReadOnlyFileException | InvalidAudioFrameException ex){}
+        portada = new Image(direccionPortada);
+    }
+    
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    
+    public void setGeneros(ArrayList<String> generos){
+        this.generos = generos;
+    }
+    
+    public void setPortada(String direccionPortada){
+       portada = new Image(direccionPortada); 
+    }
+    
+    public String getTitulo(){
+        return titulo;
+    }
+    
+    public ArrayList<String> getGeneros(){
+        return generos;
+    }
+    
+    public Image getPortada(){
+        return portada;
+    }
+    
+    public int getReproducciones(){
+        return reproducciones;
+    }
+    
+    public int getDuracion(){
+        return duracionSegundos;
+    }
+}
