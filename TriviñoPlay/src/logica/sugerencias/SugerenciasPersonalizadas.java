f92e4fe427f04f9d954a03625d6f747523c3ea36
Elías Ocque <elias.ocque2@gmail.com>
2021-06-21 01:50:49 -0400

    Empezo implementacion parcial de las sugerencias personalizadas

---
+++ TriviñoPlay/src/logica/sugerencias/SugerenciasPersonalizadas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica.sugerencias;
+
+import datos.GestorDatos;
+import logica.Cuenta;
+import logica.media.Multimedia;
+import logica.media.Serie;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ *
+ * @author Sudaii
+ */
+public class SugerenciasPersonalizadas extends Sugerencias{
+    
+    public SugerenciasPersonalizadas (GestorDatos datos, Cuenta usuario){
+        super(datos);
+    }
+    
+    private void determinarGenerosMasVistos()
+    
+}
